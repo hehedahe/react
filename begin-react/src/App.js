ef2349c61ef5b71a75bebf018b1e335b90ecc2cd
Dahae Chae <smyekgo@gmail.com>
2024-07-21 10:45:58 +0900

    벨로퍼트와 함께 하는 모던 리액트 - fragment

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import Hello from './Hello';
 
 function App(){
+  const name = 'manggom';
   return (
-    <div>
+    <>
+      <h1> Fragment: 이름 없는 태그 </h1>
+      <div> 이름 없는 태그를 사용하면 Fragment 가 만들어지는데, 이 Fragment는 브라우저 상에 별도의 엘리먼트로 나타나지 않는다!</div>
       <Hello/>
-      <Hello/>
-      <Hello/>
-      <Hello/>
-      <Hello/>
-    </div>
+    </>
   );
 }
 export default App;
